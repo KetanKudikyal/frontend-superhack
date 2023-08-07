d225d27b055585bf184f2547d5a52b033c1a3c7d
Ketan <krotusppp@gmail.com>
2023-08-08 01:27:43 +0530

    fix: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import * as React from 'react';
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Hello</h1>
+    </main>
+  );
+}
---
+++ src/constant/config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: 'SuperZapier',
+  description:
+    'An automation dev tool that allows scheduled batch payments between Base and Optimism',
+  url: 'https://superzapier.vercel.app',
+};
